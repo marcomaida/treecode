1142680c9909a5ef30f74cd3fe10ce23cf20bc69
Marco Maida <mmaida@mpi-sws.org>
2021-11-26 00:07:10 +0100

    Removed old labels

--- ui/experiments/enumeration.py
+++ ui/experiments/enumeration.py
@@ -20,17 +20,17 @@ def run(img):
 		t = converter.convert(i)
 		layout.layout_tree_fractal (t, tree_position, tools.canvasY*.25, 2, Vector(0,1))
 		
-		img = tools.draw_text(img, (50,100), f"{i}", scale=4)
+		img = tools.draw_text(img, (50,100), f"N: {i}", scale=4)
 		tools.draw_tree(img, t, 25)
 
 		# Extra data
 		ml = tree.max_layers(t)
-		next_layer = stats.treegrowth(3, ml)
+		#next_layer = stats.treegrowth(3, ml)
 		img = tools.draw_text(img, (50,200), f"Max layers: {ml}", scale=1)
-		img = tools.draw_text(img, (50,250), f"Next full tree at: { next_layer-1 }", scale=1)
-		if i == next_layer-1:		
-			img = tools.draw_text(img, (50,500), f"Full tree!", scale=4)
-			auto = False
+		#img = tools.draw_text(img, (50,250), f"Next full tree at: { next_layer-1 }", scale=1)
+		#if i == next_layer-1:		
+	    #	img = tools.draw_text(img, (50,500), f"Full tree!", scale=4)
+	    #	auto = False
 	
 		cv.imshow("tree", img)
 		key = cv.waitKey(1 if auto else -1)
@@ -39,10 +39,10 @@ def run(img):
 		elif key == ord('s'): # Stop auto
 			auto = False
 		
-		if key == ord('w'): # Fast forward
-			auto = False
-			while i < next_layer-3:
-				t = tree.successor (t, 3)
-				i +=1
+		#if key == ord('w'): # Fast forward
+		#	auto = False
+		#	while i < next_layer-3:
+		#		t = tree.successor (t, 3)
+		#		i +=1
 				
 		i+=1
