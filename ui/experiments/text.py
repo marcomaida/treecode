de5158aaa5f652643da0ea919438bb20bd70ce03
Marco Maida <mmaida@mpi-sws.org>
2021-12-18 15:15:32 +0100

    Add tree control

--- ui/experiments/text.py
+++ ui/experiments/text.py
@@ -13,31 +13,57 @@ def run(img):
 	auto = False
 	t = tree.one()
 	text = ""
-
+	tree_position = Vector(tools.canvasX/2,tools.canvasY*.8) 
+	branch_size = tools.canvasY*.07
+	tree_speed = tools.canvasX*.01
 	while True:
 		img = tools.clean_canvas()
 
-		tree_position = Vector(tools.canvasX/2,tools.canvasY*.8) 
 		stream = bsn.bit_stream_text(text)
 		t = converter.bits_to_tree(stream)
-		layout.layout_tree_fractal (t, tree_position, tools.canvasY*.07, 1.5, Vector(0,-1))
+		ml = tree.max_layers(t)
+		plbr = {}
+		tree.per_layer_breadth(t, plbr)
+		br = max (plbr.values())
+		layout.layout_tree_fractal (t, tree_position, branch_size, 1.5, Vector(0,-1))
 		
-		tools.draw_tree(img, t, 3)
+		x_width = 20
+		offsets = [-x_width * plbr[i]//2 + br*i/ml for i in range(ml)]
+		nexts = {}
+		#layout.lt_wetherell_shannon_line (t, tree_position, tools.canvasY*.07,offsets, x_width, Vector(0,-1),0,nexts)
+		
+		
+		tools.draw_tree(img, t, 1)
 
 		# Extra data
-		ml = tree.max_layers(t)
 		img = tools.draw_text(img, (50,100), f"{text}", scale=3)
 		img = tools.draw_text(img, (50,200), f"Characters: { len(text) }", scale=1)
 		img = tools.draw_text(img, (50,250), f"Bits: { len(text)*8 }", scale=1)
 		img = tools.draw_text(img, (50,300), f"Max layers: {ml}", scale=1)
 		img = tools.draw_text(img, (50,350), f"Branches: { tree.n_branches(t) }", scale=1)
+		img = tools.draw_text(img, (50,400), f"Breadth: { max(plbr.values()) }", scale=1)
 	
 		cv.imshow("tree", img)
 		key = cv.waitKey(1 if auto else -1)
-		if key == ord('2'):
+	
+		if key == ord('1'):
+			branch_size -= tree_speed
+		elif key == ord('2'):
+			branch_size += tree_speed
+		elif key == ord('3'):
 			text+=text
-		text += chr(key)
-
-		if key == 127: # backslash
+		elif key == 127: # backslash
 			text = ""
+		elif key == 0: #up
+			tree_position.y -= tree_speed
+		elif key == 1: #down
+			tree_position.y += tree_speed
+		elif key == 2: #left
+			tree_position.x -= tree_speed
+		elif key == 3: #right
+			tree_position.x += tree_speed
+		else:
+			text += chr(key)
+		
+		
 				
