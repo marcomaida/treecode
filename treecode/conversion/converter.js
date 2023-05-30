d071c645f7a11adb07e6200ab402281f926f8043
Marco <marco@wayve.ai>
2023-05-30 21:06:39 +0100

    Add wrapping to ensure ordering

--- treecode/conversion/converter.js
+++ treecode/conversion/converter.js
@@ -6,23 +6,18 @@ export function bitsToTree(stream, specs) {
     root.children = [new TreeNode(root)]
     var frontier = [root.children[0]];
     var iter = stream.getStream()
-    for (var bit of iter) { 
+    for (var bit of iter) {
         var node = frontier.shift();
 
         if (frontier.length > 0) { // node is allowed to have no children
             if (bit == 0) continue; // no child
-            else  {
+            else {
                 bit = iter.next().value; // node will have children, a 1 has been processed
                 if (bit === undefined) bit = 0 // edge case in which this was the last 1
             }
         }
-        
-        for (var i = 0; i < specs.numSegments-1; i ++) {
-            node.children = [new TreeNode(node)]
-            node = node.children[0]
-        }
 
-        if (bit === 0) 
+        if (bit === 0)
             node.children = [new TreeNode(node), new TreeNode(node)];
         else {
             console.assert(bit === 1)
@@ -33,7 +28,28 @@ export function bitsToTree(stream, specs) {
             frontier.push(c)
     }
 
+    wrap_tree(root);
     return new Tree(root, specs);
+}
+
+/**
+ * Encode child ordering in topology
+ */
+export function wrap_tree(parent_node) {
+    // TODO
+    // needs_ord = not all([tree.tree.equal(node.children[i - 1], node.children[i]) 
+    //                      for i in range(1, len(node.children))])
+
+    for (const [i, c] of parent_node.children.entries()) {
+        wrap_tree(c)
+
+        var wrap_node = c
+        for (var w = 0; w < i; w++) {
+            parent_node.children[i] = new TreeNode(parent_node, [wrap_node])
+            wrap_node.parent_node = parent_node.children[i]
+            wrap_node = parent_node.children[i]
+        }
+    }
 }
 
 export function treeToBits(tree) {
@@ -41,7 +57,7 @@ export function treeToBits(tree) {
     var frontier = [root.children[0]];
     var bits = []
 
-    while (frontier.length > 0) { 
+    while (frontier.length > 0) {
         var node = frontier.shift();
 
         // 1 - If the node has a choice, does it have children?
@@ -49,7 +65,7 @@ export function treeToBits(tree) {
             if (node.children.length === 0) bits += [0]
             else bits += [1]
         }
-        
+
         // 2 - How many children?
         if (node.children.length === 2) bits += [0]
         else if (node.children.length === 3) bits += [1]
--- treecode/tree/tree.js
+++ treecode/tree/tree.js
@@ -6,96 +6,96 @@ import { TreeSpecs } from "./tree_specs.js";
 const SEED_COLLIDER_SEGMENTS = 10
 
 export class Tree {
-    constructor(root, specs) {
-      this.root = root
-      this.specs = specs
+  constructor(root, specs) {
+    this.root = root
+    this.specs = specs
 
-      this.mesh = null
-      this.seedPosition = null
-      this.pixiMesh = null
-      this.buffer = null
-      
-      this.maxDepth = 0
+    this.mesh = null
+    this.seedPosition = null
+    this.pixiMesh = null
+    this.buffer = null
 
-      this.initialize_nodes(this.root, "X", 0)
-    }
+    this.maxDepth = 0
 
-    /**
-     * Initializes all the attributes of a node. Most of them
-     * are helpers (label, num descendants, tree)
-     * Including the node itself, i.e., a leaf node has 1 descendant
-     * */
-    initialize_nodes(node, prefix, depth) {
-      node.label = prefix
-      var num = 1
-
-      for (const [i, c] of node.children.entries()) {
-        this.initialize_nodes(c, prefix + i.toString(), depth+1)
-        num += c.numDescendants
-      }
-      node.numDescendants = num
-      node.tree = this
-
-      this.maxDepth = Math.max(this.maxDepth, depth)
-    }
-
-    initializeMesh(app, position) {
-      const vertices = create_tree_mesh(this)
-
-      const geometry = new PIXI.Geometry()
-                              .addAttribute('aVertexPosition', vertices)
-      this.pixiMesh = new PIXI.Mesh(geometry, getShader())
-      this.buffer = this.pixiMesh.geometry.getBuffer('aVertexPosition');
-      this.pixiMesh.position.set(position.x, position.y)
-      app.stage.addChild(this.pixiMesh)
-      this.mesh = this.buffer.data
-
-      drawSeed(this)
-    }
-
-    /* If multiple nodes are moved together,
-       the mesh may be broken. This function fixes
-       this behavior */
-    refresh_nodes(t) {
-      this._refresh_nodes(t.root)
-      drawSeed(this)
-    }
-
-    _refresh_nodes(node) {
-      node.setPosition(node.position)
-      for (const c of node.children) this._refresh_nodes(c)
-    }
-
-    moveMeshPos(vec) {
-      vec.add(new PIXI.Vector(this.pixiMesh.position.x, this.pixiMesh.position.y))
-      this.pixiMesh.position.set(vec.x, vec.y)
-    }
-
-    setMeshPos(pos) {
-      this.pixiMesh.position.set(pos.x, pos.y)
-    }
-
-    destroyMesh(app) {
-      app.stage.removeChild(this.pixiMesh)
-    }
-
-    seedCollider() {
-      var r = SEED_RING_OUTER_RADIUS //TODO add padding?
-      this.root.colliderPolygon = circlePolygon(this.root.position, r, SEED_COLLIDER_SEGMENTS)
-    }
-
-    transformPosition(vec) {
-      return vec.add(new PIXI.Vector(this.pixiMesh.position.x, this.pixiMesh.position.y))
-    }
+    this.initialize_nodes(this.root, "X", 0)
   }
 
-export function* treeIterator(tree) {
-    var node = tree.root
-    var frontier = [node]
+  /**
+   * Initializes all the attributes of a node. Most of them
+   * are helpers (label, num descendants, tree)
+   * Including the node itself, i.e., a leaf node has 1 descendant
+   */
+  initialize_nodes(node, prefix, depth) {
+    node.label = prefix
+    var num = 1
 
-    while (frontier.length > 0) {
-      node = frontier.shift()
-      frontier.push(...node.children)
-      yield node
+    for (const [i, c] of node.children.entries()) {
+      this.initialize_nodes(c, prefix + i.toString(), depth + 1)
+      num += c.numDescendants
     }
+    node.numDescendants = num
+    node.tree = this
+
+    this.maxDepth = Math.max(this.maxDepth, depth)
+  }
+
+  initializeMesh(app, position) {
+    const vertices = create_tree_mesh(this)
+
+    const geometry = new PIXI.Geometry()
+      .addAttribute('aVertexPosition', vertices)
+    this.pixiMesh = new PIXI.Mesh(geometry, getShader())
+    this.buffer = this.pixiMesh.geometry.getBuffer('aVertexPosition');
+    this.pixiMesh.position.set(position.x, position.y)
+    app.stage.addChild(this.pixiMesh)
+    this.mesh = this.buffer.data
+
+    drawSeed(this)
+  }
+
+  /* If multiple nodes are moved together,
+     the mesh may be broken. This function fixes
+     this behavior */
+  refresh_nodes(t) {
+    this._refresh_nodes(t.root)
+    drawSeed(this)
+  }
+
+  _refresh_nodes(node) {
+    node.setPosition(node.position)
+    for (const c of node.children) this._refresh_nodes(c)
+  }
+
+  moveMeshPos(vec) {
+    vec.add(new PIXI.Vector(this.pixiMesh.position.x, this.pixiMesh.position.y))
+    this.pixiMesh.position.set(vec.x, vec.y)
+  }
+
+  setMeshPos(pos) {
+    this.pixiMesh.position.set(pos.x, pos.y)
+  }
+
+  destroyMesh(app) {
+    app.stage.removeChild(this.pixiMesh)
+  }
+
+  seedCollider() {
+    var r = SEED_RING_OUTER_RADIUS //TODO add padding?
+    this.root.colliderPolygon = circlePolygon(this.root.position, r, SEED_COLLIDER_SEGMENTS)
+  }
+
+  transformPosition(vec) {
+    return vec.add(new PIXI.Vector(this.pixiMesh.position.x, this.pixiMesh.position.y))
+  }
+}
+
+export function* treeIterator(tree) {
+  var node = tree.root
+  var frontier = [node]
+
+  while (frontier.length > 0) {
+    node = frontier.shift()
+    frontier.push(...node.children)
+    yield node
+  }
 }
