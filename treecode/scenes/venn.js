f3fcaa9e898e13a09f1e46fd86f70c14b77c44a3
Marco <marco@wayve.ai>
2023-05-31 04:08:58 +0200

    Lay out children on longer dimension

--- treecode/scenes/venn.js
+++ treecode/scenes/venn.js
@@ -40,26 +40,52 @@ function drawTree(node, bar_frame, color) {
     const bar_height = bar_frame[3].y - bar_frame[0].y
 
     /* A node should take exactly as much horizontal and vertical space as 1/numDescendants */
-    const parent_rel_space = 1 / node.numDescendants
-    const parent_global_rel_space = 1 / (node.tree.maxDepth + 1)
-    const y_margin = parent_global_rel_space * bar_height / 2 // There are always two vertical margins
-    const x_margin = parent_rel_space * bar_width / (1 + node.children.length) // num of vertical bars
+    const parent_rel_space = (1 / node.numDescendants) ** (0.65)
+    //const parent_rel_space = 1 / (node.tree.maxDepth + 1)
 
-    let top_left = bar_frame[0].clone().add(new PIXI.Vector(x_margin, y_margin))
+    if (bar_width >= bar_height) {
+        // Horizontal layout
+        const inter_margin = parent_rel_space * bar_width / (1 + node.children.length) // num of vertical bars
+        const side_margin = parent_rel_space * bar_height / 2 // There are always two side margins
 
-    for (let c of node.children) {
-        const child_weight = c.numDescendants / (node.numDescendants - 1)
-        const child_width = bar_width * (1. - parent_rel_space) * child_weight
-        const child_height = bar_height * (1. - parent_global_rel_space)
-        var child_frame = [
-            top_left.clone(),
-            top_left.clone().add(new PIXI.Vector(child_width, 0)),
-            top_left.clone().add(new PIXI.Vector(child_width, child_height)),
-            top_left.clone().add(new PIXI.Vector(0, child_height))
-        ]
+        let top_left = bar_frame[0].clone().add(new PIXI.Vector(inter_margin, side_margin))
 
-        drawTree(c, child_frame, inverse_color(color))
-        top_left.add(new PIXI.Vector(child_width + x_margin, 0))
+        for (let c of node.children) {
+            const child_weight = c.numDescendants / (node.numDescendants - 1)
+            const child_width = bar_width * (1. - parent_rel_space) * child_weight
+            const child_height = bar_height * (1. - parent_rel_space)
+            var child_frame = [
+                top_left.clone(),
+                top_left.clone().add(new PIXI.Vector(child_width, 0)),
+                top_left.clone().add(new PIXI.Vector(child_width, child_height)),
+                top_left.clone().add(new PIXI.Vector(0, child_height))
+            ]
+
+            drawTree(c, child_frame, inverse_color(color))
+            top_left.add(new PIXI.Vector(child_width + inter_margin, 0))
+        }
+    }
+    else {
+        // vertical layout
+        const side_margin = parent_rel_space * bar_width / 2 // There are always two side margins
+        const inter_margin = parent_rel_space * bar_height / (1 + node.children.length) // num of horizontal bars
+
+        let top_left = bar_frame[0].clone().add(new PIXI.Vector(side_margin, inter_margin))
+
+        for (let c of node.children) {
+            const child_weight = c.numDescendants / (node.numDescendants - 1)
+            const child_height = bar_height * (1. - parent_rel_space) * child_weight
+            const child_width = bar_width * (1. - parent_rel_space)
+            var child_frame = [
+                top_left.clone(),
+                top_left.clone().add(new PIXI.Vector(child_width, 0)),
+                top_left.clone().add(new PIXI.Vector(child_width, child_height)),
+                top_left.clone().add(new PIXI.Vector(0, child_height))
+            ]
+
+            drawTree(c, child_frame, inverse_color(color))
+            top_left.add(new PIXI.Vector(0, child_height + inter_margin))
+        }
     }
 }
 
@@ -73,8 +99,10 @@ function updateCode() {
     const window_width = window.innerWidth
     const window_height = window.innerHeight
 
-    const bar_width = window_width / 1.5
-    const bar_height = window_height / 3
+    const shorter = Math.min(window_width, window_height)
+    const bar_width = shorter / 1.5
+    const bar_height = shorter / 1.5
+
     const bar_left = window_width / 2 - bar_width / 2
     const bar_top = window_height / 2 - bar_height / 2
 
